67fded690028c3c9917769e630d571b52a3b96f4
magnuspaal <magnuspaal@gmail.com>
2024-03-06 14:48:00 +0200

    test: init tests

--- src/test/java/com/magnuspaal/messagingservice/MessagingServiceApplicationTests.java
+++ src/test/java/com/magnuspaal/messagingservice/MessagingServiceApplicationTests.java
@@ -1,10 +1,39 @@
 package com.magnuspaal.messagingservice;
 
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.util.TestPropertyValues;
+import org.springframework.context.ApplicationContextInitializer;
+import org.springframework.context.ConfigurableApplicationContext;
+import org.springframework.test.context.ContextConfiguration;
+import org.springframework.test.context.junit.jupiter.SpringExtension;
+import org.testcontainers.containers.PostgreSQLContainer;
+import org.testcontainers.junit.jupiter.Container;
+import org.testcontainers.junit.jupiter.Testcontainers;
 
-@SpringBootTest
+@ExtendWith(SpringExtension.class)
+@SpringBootTest(classes = MessagingServiceApplicationTests.class)
+@ContextConfiguration(initializers = {MessagingServiceApplicationTests.Initializer.class})
+@Testcontainers
 class MessagingServiceApplicationTests {
+
+	@Container
+	public static PostgreSQLContainer<?> postgreSQLContainer = new PostgreSQLContainer("postgres:11.1")
+			.withDatabaseName("messaging-service-test")
+			.withUsername("username")
+			.withPassword("password");
+
+	static class Initializer
+			implements ApplicationContextInitializer<ConfigurableApplicationContext> {
+		public void initialize(ConfigurableApplicationContext configurableApplicationContext) {
+			TestPropertyValues.of(
+					"spring.datasource.url=" + postgreSQLContainer.getJdbcUrl(),
+					"spring.datasource.username=" + postgreSQLContainer.getUsername(),
+					"spring.datasource.password=" + postgreSQLContainer.getPassword()
+			).applyTo(configurableApplicationContext.getEnvironment());
+		}
+	}
 
 	@Test
 	void contextLoads() {
